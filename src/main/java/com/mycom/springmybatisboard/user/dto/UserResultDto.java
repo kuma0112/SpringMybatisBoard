a0a2f14f34c8a6db5eb02a60ba83729941664d50
kuma0112 <lsj23871@gmail.com>
2024-08-13 17:39:23 +0900

    회원가입 결과 처리 정보를 담고 있는 UserResultDto

---
+++ src/main/java/com/mycom/springmybatisboard/user/dto/UserResultDto.java
@@ -0,0 +1,13 @@
+package com.mycom.springmybatisboard.user.dto;
+
+public class UserResultDto {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
