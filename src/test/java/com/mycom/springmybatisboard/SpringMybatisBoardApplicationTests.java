c17286c0ba18d36d995b17867e250ad2672d58f3
kuma0112 <lsj23871@gmail.com>
2024-08-14 09:17:51 +0900

    main

---
+++ src/test/java/com/mycom/springmybatisboard/SpringMybatisBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.springmybatisboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
